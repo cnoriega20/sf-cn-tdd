579e67c02ceac07b414450a671ee66ef90a5bb93
cnoriega20 <c79.noriega@gmail.com>
2020-09-29 03:39:38 -0500

    Initial commit: Spring initialzr structure.

---
+++ src/main/java/com/springboot/tdd/SfCnTddApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.tdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfCnTddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfCnTddApplication.class, args);
+    }
+
+}
