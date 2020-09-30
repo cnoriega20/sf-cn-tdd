a111429572b8eafd4b8ad7ebfe7b2cf0135f99e1
cnoriega20 <c79.noriega@gmail.com>
2020-09-30 00:40:28 -0500

    Fixed assertion Failure. Added repository reference and usage to the Service Implementation method.

--- src/main/java/com/springboot/tdd/services/ToDoServiceImpl.java
+++ src/main/java/com/springboot/tdd/services/ToDoServiceImpl.java
@@ -1,16 +1,22 @@
 package com.springboot.tdd.services;
 
 import com.springboot.tdd.domain.ToDo;
+import com.springboot.tdd.repositories.ToDoRepository;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class ToDoServiceImpl implements ToDoService {
 
+    private final ToDoRepository toDoRepository;
+
+    public ToDoServiceImpl(ToDoRepository toDoRepository) {
+        this.toDoRepository = toDoRepository;
+    }
+
     @Override
     public List<ToDo> getAllTodos() {
-        return new ArrayList<>();
+        return toDoRepository.findAll();
     }
 }
